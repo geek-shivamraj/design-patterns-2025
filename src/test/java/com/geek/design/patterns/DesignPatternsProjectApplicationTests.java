0968ac0e3910b153cbd8e1b71d45b13c92784b09
Shivam-Omen <srvcode@gmail.com>
2025-04-12 15:32:54 +0530

    Adding basic template files

---
+++ src/test/java/com/geek/design/patterns/DesignPatternsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.design.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
